c32dddd07210992c87e48ce8c1044bc1852bbc65
cursoDesenvWeb <gabriel.santana106@yahoo.com.br>
2015-01-21 06:07:44 -0300

    Classe Produto

---
+++ Dominio/Entidades/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Entidades
+{
+   public class Produto
+    {
+       public int Id { get; set; }
+       public String Nome { get; set; }
+       public String Descricao { get; set; }
+       public decimal preco { get; set; }
+
+
+    }
+}
