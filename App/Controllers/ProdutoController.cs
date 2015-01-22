0ef08300f033767aa11a725a7bce5fb58ffd36fe
cursoDesenvWeb <gabriel.santana106@yahoo.com.br>
2015-01-21 22:10:31 -0300

    Persistencia

---
+++ App/Controllers/ProdutoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Dominio.Entidades;
+using Dominio.Repositorio;
+
+namespace App.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private ProdutoRepositorio _repositorio;
+        // GET: Produto
+        public ActionResult Index()
+        {
+            _repositorio = new ProdutoRepositorio();
+            var produtos = _repositorio.Produtos;
+            return View(produtos);
+        }
+    }
+}
---
+++ Dominio/Repositorio/ProdutoRepositorio.cs
@@ -0,0 +1,22 @@
+﻿using Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Dominio.Repositorio
+{
+    public class ProdutoRepositorio
+    {
+        private readonly EfDbContext _context = new EfDbContext();
+
+        public IEnumerable<Produto> Produtos
+        {
+            get { return _context.Produtos; }
+        }
+
+        
+    }
+}
